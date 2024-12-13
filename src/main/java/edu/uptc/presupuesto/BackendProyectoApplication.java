6f5131d35c4d8e6aab19958acdac7268a128bad0
Andres Felipe Mateus Riaño <105401959+AndresMate@users.noreply.github.com>
2024-12-13 15:18:07 -0500

    Create repo

---
+++ src/main/java/edu/uptc/presupuesto/BackendProyectoApplication.java
@@ -0,0 +1,13 @@
+package edu.uptc.presupuesto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendProyectoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendProyectoApplication.class, args);
+    }
+
+}
