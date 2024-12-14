4648065f3fe52f0082eb6b30bee8e78913fbc0ee
Andres Felipe Mateus Riaño <105401959+AndresMate@users.noreply.github.com>
2024-12-14 13:09:44 -0500

    Update UsuarioDTO.java

--- src/main/java/edu/uptc/presupuesto/dto/UsuarioDTO.java
+++ src/main/java/edu/uptc/presupuesto/dto/UsuarioDTO.java
@@ -1,14 +1,30 @@
 package edu.uptc.presupuesto.dto;
 
 import edu.uptc.presupuesto.model.Usuario.Rol;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import java.time.LocalDateTime;
 
 public class UsuarioDTO {
+    @NotNull(message = "El ID es obligatorio")
     private Long id;
+
+    @NotBlank(message = "El nombre de usuario es obligatorio")
     private String username;
+
+    @NotBlank(message = "El correo electrónico es obligatorio")
+    @Email(message = "El correo electrónico no es válido")
     private String email;
+
+    @NotNull(message = "El rol es obligatorio")
     private Rol rol;
+
+    @NotNull(message = "El estado de activación es obligatorio")
     private boolean activo;
+
+    @NotNull(message = "La fecha de último acceso es obligatoria")
     private LocalDateTime ultimoAcceso;
 
     // Getters y Setters
